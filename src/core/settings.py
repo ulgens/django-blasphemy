c1b2c51e00fdff18da11d17ae856666c367d43b5
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2021-06-07 01:59:00 +0300

    Init django-environ

--- src/core/settings.py
+++ src/core/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+# TODO:
+#  Compare with dynaconf
+#  https://dynaconf.readthedocs.io/en/latest/
+env = environ.Env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
