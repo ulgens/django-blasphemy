721e6cdb279cac4a47be43a4439be9355fe77a2a
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2022-01-17 07:53:23 +0300

    Apply black

--- src/core/views.py
+++ src/core/views.py
@@ -4,13 +4,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def error(request):
     1 / 0
 
 
 def manual_log(request):
     try:
-        1/0
+        1 / 0
     except Exception as error:
         logger.exception(error)
         raise error
@@ -18,7 +19,7 @@ def manual_log(request):
 
 def manual_flat_log(request):
     try:
-        1/0
+        1 / 0
     except Exception as error:
         logger.exception(str(error))
         raise error
