5465e43665c3ff29b8dc7f87ef8d8468764e5a71
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2022-01-23 13:27:40 +0300

    Add static config

--- src/core/settings.py
+++ src/core/settings.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/dev/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/topics/settings/
 """
-
+import os
 from pathlib import Path
 
 import dj_database_url
@@ -140,6 +140,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
