f5a82f4f99b9dedade9492c5833d9d826cd38dff
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2022-01-23 13:36:19 +0300

    Add whitenoise for Heroku

--- src/core/settings.py
+++ src/core/settings.py
@@ -63,6 +63,7 @@ if DEBUG:
     INSTALLED_APPS += DEBUG_APPS
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -144,6 +145,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
