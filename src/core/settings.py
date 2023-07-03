da04e1d8410e9bfe0c5b9280d08420c0f48ae548
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2023-07-03 15:51:28 +0300

    Handle package updates

--- src/core/settings.py
+++ src/core/settings.py
@@ -151,7 +151,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 # Default primary key field type
