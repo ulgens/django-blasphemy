b91a754027ba410a7808d35942a836726338594a
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2024-03-26 16:51:55 +0300

    Init Snippet model

---
+++ src/snippets/models.py
@@ -0,0 +1,26 @@
+from core.models import BaseModel
+from django.db import models
+
+from .choices import LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class Snippet(BaseModel):
+    title = models.CharField(
+        max_length=128,
+        blank=True,
+        default="",
+    )
+    code = models.TextField()
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES,
+        default="python",
+        max_length=128,
+    )
+    style = models.CharField(
+        choices=STYLE_CHOICES,
+        default="friendly",
+        max_length=128,
+    )
+
+    class Meta:
+        ordering = ("-created_at",)
