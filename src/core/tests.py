efe2cb227858003a5cfc6cb9de90d0e70345c022
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2024-07-06 17:59:55 +0300

    Check there is no pending migrations

---
+++ src/core/tests.py
@@ -0,0 +1,15 @@
+from io import StringIO
+
+from django.core.management import call_command
+from django.test import TestCase
+
+
+# Thanks to https://adamj.eu/tech/2024/06/23/django-test-pending-migrations/
+class PendingMigrationsTests(TestCase):
+    def test_no_pending_migrations(self):
+        out = StringIO()
+
+        try:
+            call_command("makemigrations", "--check", stdout=out)
+        except SystemExit:
+            raise AssertionError("Pending migrations:\n" + out.getvalue()) from None
