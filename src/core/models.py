1458bb9c4cb964aa68c0b1be5389ce3b07ce03ff
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2024-07-15 06:40:23 +0300

    Add DirtyFieldsMixin to BaseModel
    This helps a lot when one needs to understand which fields of a model has changed

--- src/core/models.py
+++ src/core/models.py
@@ -1,4 +1,5 @@
 import uuid_extensions
+from dirtyfields import DirtyFieldsMixin
 from django.db import models
 
 __all__ = ("BaseModel",)
@@ -28,6 +29,32 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
-class BaseModel(TimeStampedModel, UUIDModel):
+class BaseModel(DirtyFieldsMixin, TimeStampedModel, UUIDModel):
     class Meta:
         abstract = True
+
+    def save_dirty_fields(self):
+        """
+        Save only the dirty fields.
+
+        Fields with auto_now=True never gets dirty.
+        We need to add them to the update_fields list manually if anything else is dirty.
+
+        https://github.com/romgar/django-dirtyfields/issues/195
+        """
+
+        if self._state.adding:
+            self.save()
+            return
+
+        dirty_fields = self.get_dirty_fields(check_relationship=True)
+        update_fields = list(dirty_fields.keys())
+
+        if not update_fields:
+            return
+
+        # auto_now fields never get dirty, they need to be added manually
+        auto_now_fields = [f.name for f in self._meta.fields if getattr(f, "auto_now", False)]
+        update_fields.extend(auto_now_fields)
+
+        self.save(update_fields=update_fields)
