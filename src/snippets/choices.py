419f26a22182d68871afd26c13e764038a5e7d4f
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2024-07-17 21:29:01 +0300

    Re-write choice comprehensions and fix value - label order

--- src/snippets/choices.py
+++ src/snippets/choices.py
@@ -1,24 +1,5 @@
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
-LEXERS = get_all_lexers()
-# TODO: Convert this to a choices class
-LANGUAGE_CHOICES = []
-
-for lexer in LEXERS:
-    name = lexer[0]
-    aliases = lexer[1]
-    alias = aliases[0] if aliases else name.lower()
-
-    LANGUAGE_CHOICES.append((name, alias))
-
-
-STYLES = get_all_styles()
-# TODO: Convert this to a choices class
-STYLE_CHOICES = []
-
-for style in STYLES:
-    name = style
-    alias = style.lower()
-
-    STYLE_CHOICES.append((name, alias))
+LANGUAGE_CHOICES = [(values[0] if values else label, label) for label, values, *_ in get_all_lexers()]
+STYLE_CHOICES = [(value, value) for value in get_all_styles()]
