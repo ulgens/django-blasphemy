d500addc82602d5c398a0809751613a1df2d87a2
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2024-07-26 16:52:54 +0300

    Reorganize users' admin files

---
+++ src/users/admin/__init__.py
@@ -0,0 +1,1 @@
+from .model_admins import *  # noqa: F403
--- src/users/admin/model_admins.py
+++ src/users/admin/model_admins.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User
+User = get_user_model()
 
 
 @admin.register(User)
