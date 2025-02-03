fe61b6b6b0393fc46f921cf8ee124a45f830a177
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-02-03 09:26:15 +0300

    Don't shadow imported module

--- src/core/settings.py
+++ src/core/settings.py
@@ -210,7 +210,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 if DEBUG:
     import socket
 
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    hostname, aliases, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
 
