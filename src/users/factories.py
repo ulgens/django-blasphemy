37e39ad8610b7e8e48e75a506833a6d471a87950
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-05-14 18:54:01 +0300

    Generate valid emails in UserFactory

--- src/users/factories.py
+++ src/users/factories.py
@@ -13,10 +13,26 @@ User = get_user_model()
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-    # Using 'free_email' alone can cause duplicate data.
-    email = factory.LazyFunction(lambda: f"{fake.random_number(digits=3)}{fake.free_email()}")
     phone_number = factory.LazyAttribute(lambda _: fake.e164())
     password = factory.django.Password(fake.password(length=16))
+
+    @factory.lazy_attribute
+    def email(self):
+        """
+        Adds a random number suffix to the local part of the email to ensure uniqueness.
+        Using 'free_email' alone can cause duplicate data.
+
+        factory.Transformer is not used here because we don't want to transform the value if it's explicitly set.
+        """
+
+        email = fake.free_email()
+
+        email_parts = email.split("@")
+        number_suffix = fake.random_number(digits=3)
+
+        unique_email = f"{email_parts[0]}{number_suffix}@{email_parts[1]}"
+
+        return unique_email
 
     @classmethod
     def _create(cls, model_class, *args, **kwargs):
