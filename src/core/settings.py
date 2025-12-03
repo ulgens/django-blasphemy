044e9037477e4b359b4b64e36a5bf13bcfc2f181
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-12-03 21:19:42 +0300

    Apply django-upgrade fixes

--- src/core/settings.py
+++ src/core/settings.py
@@ -214,10 +214,6 @@ if TESTING:
     PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)
 
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 # Django debug toolbar
 if DEBUG:
     import socket
