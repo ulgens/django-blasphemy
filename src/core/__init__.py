e8076972b641bfb50f6454a3e434edc50edaa098
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-03-07 09:26:14 +0300

    Patch the migration writer
    So far, it uses the default values & approach, copied from django.db.migrations.writer.
    Customizations will be applied in the following commits.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,5 +1,6 @@
 # Apply monkey patches
-import core.patch_formatters  # noqa: F401
+import core.patch_formatters
+import core.patch_migration_writer  # noqa: F401
 
 # Celery app needs to be imported for its initialization
 from .celery import app as celery_app
