aea33827e7cd220ffc0732e60b4a16cda1452431
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-03-18 01:18:19 +0300

    Reset migrations

--- src/users/migrations/0001_initial.py
+++ src/users/migrations/0001_initial.py
@@ -1,10 +1,12 @@
 import uuid
 
+import dirtyfields.dirtyfields
 import django.utils.timezone
-from dirtyfields import DirtyFieldsMixin
+import phonenumber_field.modelfields
+from django.contrib.postgres.operations import CreateCollation
 from django.db import migrations, models
 
-from ..managers import UserManager
+import users.managers
 
 
 class Migration(migrations.Migration):
@@ -13,64 +15,26 @@ class Migration(migrations.Migration):
     dependencies = (("auth", "0012_alter_user_first_name_max_length"),)
 
     operations = (
+        # Creates a collation for case-insensitive text in PostgreSQL,
+        # to replace Django's CI* fields.
+        # https://adamj.eu/tech/2023/02/23/migrate-django-postgresql-ci-fields-case-insensitive-collation
+        CreateCollation(
+            "case_insensitive",
+            provider="icu",
+            locale="und-u-ks-level2",
+            deterministic=False,
+        ),
         migrations.CreateModel(
             name="User",
             fields=[
-                (
-                    "id",
-                    models.UUIDField(
-                        default=uuid.uuid7,
-                        editable=False,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                (
-                    "password",
-                    models.CharField(
-                        max_length=128,
-                        verbose_name="password",
-                    ),
-                ),
-                (
-                    "last_login",
-                    models.DateTimeField(
-                        blank=True,
-                        null=True,
-                        verbose_name="last login",
-                    ),
-                ),
-                (
-                    "full_name",
-                    models.CharField(
-                        blank=True,
-                        max_length=200,
-                        verbose_name="full name",
-                    ),
-                ),
-                (
-                    "short_name",
-                    models.CharField(
-                        blank=True,
-                        max_length=50,
-                        verbose_name="short name",
-                    ),
-                ),
+                ("password", models.CharField(max_length=128, verbose_name="password")),
+                ("last_login", models.DateTimeField(blank=True, null=True, verbose_name="last login")),
                 (
                     "is_superuser",
                     models.BooleanField(
                         default=False,
                         help_text="Designates that this user has all permissions without explicitly assigning them.",
                         verbose_name="superuser status",
-                    ),
-                ),
-                (
-                    "email",
-                    models.EmailField(
-                        max_length=254,
-                        unique=True,
-                        verbose_name="email address",
                     ),
                 ),
                 (
@@ -89,25 +53,32 @@ class Migration(migrations.Migration):
                         verbose_name="active",
                     ),
                 ),
+                ("date_joined", models.DateTimeField(default=django.utils.timezone.now, verbose_name="date joined")),
                 (
-                    "date_joined",
-                    models.DateTimeField(
-                        default=django.utils.timezone.now,
-                        verbose_name="date joined",
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid7,
+                        editable=False,
+                        help_text="The unique identifier of the record, in UUIDv7 format.",
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "email",
+                    models.EmailField(
+                        db_collation="case_insensitive", max_length=254, unique=True, verbose_name="email address"
                     ),
                 ),
                 (
-                    "created_at",
-                    models.DateTimeField(
-                        auto_now_add=True,
-                    ),
+                    "phone_number",
+                    phonenumber_field.modelfields.PhoneNumberField(blank=True, max_length=16, region=None),
                 ),
-                (
-                    "updated_at",
-                    models.DateTimeField(
-                        auto_now=True,
-                    ),
-                ),
+                ("full_name", models.CharField(blank=True, max_length=200, verbose_name="full name")),
+                ("short_name", models.CharField(blank=True, max_length=50, verbose_name="short name")),
                 (
                     "groups",
                     models.ManyToManyField(
@@ -132,12 +103,14 @@ class Migration(migrations.Migration):
                 ),
             ],
             options={
+                "verbose_name": "user",
+                "verbose_name_plural": "users",
                 "abstract": False,
                 "swappable": "AUTH_USER_MODEL",
             },
-            bases=(DirtyFieldsMixin, models.Model),
+            bases=(dirtyfields.dirtyfields.DirtyFieldsMixin, models.Model),
             managers=[
-                ("objects", UserManager()),
+                ("objects", users.managers.UserManager()),
             ],
         ),
     )
