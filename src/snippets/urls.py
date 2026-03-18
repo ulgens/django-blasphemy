77fa8fadae5ca25aed07261605067548e67842bc
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-03-18 15:04:44 +0300

    Switch to SimpleRouter
    We aren't using any of the niceties coming with DefaultRouter.
    
    > additionally includes a default API root view, that returns a response containing hyperlinks to all the list views.
    
    This is slightly useful for API documentation cases are already handled by Swagger (drf-spectacular)
    
    https://www.django-rest-framework.org/api-guide/routers/

--- src/snippets/urls.py
+++ src/snippets/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 from .api.views import SnippetViewSet
 
 __all__ = ("urlpatterns",)
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register("", SnippetViewSet, basename="snippet")
 
 urlpatterns = [
