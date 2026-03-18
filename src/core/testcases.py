e69ada14740f76747dad1f1c4f4bdcab52d00bfd
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-03-18 16:06:15 +0300

    Add not allowed method base tests to core.testcases.APITestCase

--- src/core/testcases.py
+++ src/core/testcases.py
@@ -1,8 +1,12 @@
+from typing import Literal
 from unittest import SkipTest
 
+from django.db import connection
 from django.test import Client
 from django.test import TestCase as BaseTestCase
+from django.test.utils import CaptureQueriesContext
 from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase as BaseAPITestCase
 from unittest_parametrize import ParametrizedTestCase
 
@@ -72,5 +76,51 @@ class AdminTestCase(TestCase):
         )
 
 
+checked_methods_type = Literal["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
 class APITestCase(ParametrizedTestCase, BaseAPITestCase):
-    pass
+    @classmethod
+    def setUpClass(cls):
+        if cls.__name__ == "APITestCase":
+            skip_msg = "APITestCase is an abstract base class"
+            raise SkipTest(skip_msg)
+
+        super().setUpClass()
+
+        cls.detail_url: str = ""
+        cls.detail_not_allowed_methods: list[checked_methods_type] = []
+        cls.list_url: str = ""
+        cls.list_not_allowed_methods: list[checked_methods_type] = []
+
+    def _assert_not_allowed_methods(
+        self,
+        url: str,
+        methods: list[checked_methods_type],
+    ) -> None:
+        for method in methods:
+            with CaptureQueriesContext(connection) as db_ctx:
+                # Run the request
+                resp = self.client.generic(method, url)
+
+            # Check the response
+            self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+            # Check the db queries
+            self.assertEqual(len(db_ctx), 0)
+
+    # TODO: Can this be parameterized?
+    # TODO: What happens when not allowed method list is empty? Still showing on the test report?
+    def test_detail_not_allowed_methods(self):
+        self._assert_not_allowed_methods(
+            url=self.detail_url,
+            methods=self.detail_not_allowed_methods,
+        )
+
+    # TODO: Can this be parameterized?
+    # TODO: What happens when not allowed method list is empty? Still showing on the test report?
+    def test_list_not_allowed_methods(self):
+        self._assert_not_allowed_methods(
+            url=self.list_url,
+            methods=self.list_not_allowed_methods,
+        )
