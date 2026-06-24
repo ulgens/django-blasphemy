6f8fab5883d76a1956b38b691b2ccc97f78406e5
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-06-24 15:33:43 +0300

    Move ALLOWED_HOSTS setting to the env

--- src/core/settings/all.py
+++ src/core/settings/all.py
@@ -36,7 +36,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definitions
